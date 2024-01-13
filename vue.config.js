186632e785e37ec3389b7e1f6576342bb6801e13
honyx <honyx22@gmail.com>
2024-01-13 15:49:35 +0100

    tvoje zpráva

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    disableHostCheck: true,
+    port: process.env.DEVELOPMENT_PORT || 8080,
+  },
 })
