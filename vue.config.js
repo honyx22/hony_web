0be96184a60bf8b3e97062ccf44f786049fd7123
Gumernus <gumernusbussines@gmail.com>
2024-01-27 21:48:54 +0100

    cleared

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    allowedHosts: "all",
-    port: process.env.DEVELOPMENT_PORT || 8080,
-  },
-})
